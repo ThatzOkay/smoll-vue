9773130bd6323bba2c7ba6b6aab55e084939d6eb
ThatzOkay <noduhgamingxd@gmail.com>
2025-06-17 11:44:12 +0200

    Update packages and new name

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# [1.1.0](https://github.com/thatzokay/smoll-vue/compare/v0.4.1...v1.1.0) (2025-06-17)
+
+### Bug Fixes
+
+- fix pico-vue dependcies to 3.2.27 ([72ff52c](https://github.com/thatzokay/smoll-vue/commit/72ff52cc479b9c6600104a74e826754294ad45d9))
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+  },
   build: {
     target: 'esnext',
     minify: 'terser',
